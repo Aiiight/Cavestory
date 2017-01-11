a6dedf892acf47fc79143f9ad22062f2ee1ec031
Abu <akhawaja@ramapo.edu>
2017-01-10 23:40:56 -0500

    Add files via upload

---
+++ Cavestory/animatedsprite.h
@@ -0,0 +1,55 @@
+#ifndef ANIMATED_SPRITE_H
+#define ANIMATED_SPRITE_H
+
+#include<map>
+#include<string>
+#include<vector>
+#include "sprite.h"
+#include"globals.h"
+
+class Graphics;
+
+/* holds logic for animating sprites*/
+
+class AnimatedSprite : public Sprite
+{
+public:
+	AnimatedSprite();
+	AnimatedSprite(Graphics &graphcs, const std::string &filePath, int sourceX, int sourceY, int width, int height,
+		float posX, float posY, float timeToUpdate);
+
+	void playAnimation(std::string animation, bool once = false);
+
+	void update(int elapsedTime);
+
+	void draw(Graphics &graphics, int x, int y);
+
+
+	// void setupAnimation: required function that sets up all animations for a sptire
+	virtual void setupAnimation();
+
+protected:
+	double timeToUpdate;
+	bool currentAnimationOnce;
+	std::string currentAnimation;
+
+	//addanimation adds an animation to the map o fanimations for the sprite
+	void addAnimation(int frames, int x, int y, std::string name, int width, int height, Vector2 offset);
+
+	void resetAnimations();
+
+	void stopAnimation();
+
+	void setVisible(bool visible);
+
+	virtual void animationDone(std::string currentAnimation);
+private:
+	std::map<std::string, std::vector<SDL_Rect> > animations;
+	std::map<std::string, Vector2> offsets;
+
+	int frameIndex;
+	double timeElapsed = 0;
+	bool visible;
+};
+
+#endif
